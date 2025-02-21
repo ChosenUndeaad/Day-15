8fb9f86b7817b74e1e5f135eab69308b427bf617
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-02-21 11:58:38 +0100

    Updated

--- D10.js
+++ D10.js
@@ -245,21 +245,61 @@ console.log("Is it my birthday today?", isTodayMyBirthday());
   in esso la proprietà chiamata come la stringa passata come secondo parametro.
 */
 
+const deleteProp = function (obj, property) {
+  delete obj[property];
+  return obj;
+};
+
+let myObj = {
+  name: "Slim Shady",
+  "is he back?": "yes",
+  favFood: "Mom's spaghetti",
+};
+
+console.log(deleteProp(myObj, "is he back?"));
+
 /* ESERCIZIO 12
   Scrivi una funzione chiamata "newestMovie" che trova il film più recente nell'array "movies" fornito.
 */
+
+const newestMovie = function (movies) {
+  let newest = movies[0];
+
+  movies.forEach((movie) => {
+    if (movie.Year > newest.Year) {
+      newest = movie;
+    }
+  });
+
+  return newest;
+};
 
 /* ESERCIZIO 13
   Scrivi una funzione chiamata countMovies che ritorna il numero di film contenuti nell'array "movies" fornito.
 */
 
+const countMovies = function (movies) {
+  let moviesNumber = movies.length;
+  return moviesNumber;
+};
+
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
 
+const onlyTheYears = function (movies) {
+  return movies.map((movie) => movie.Year);
+};
+
 /* ESERCIZIO 15
   Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
 */
+
+const onlyInLastMillennium = function (movies) {
+  return movies
+    .filter((movie) => movie.Year >= 1901 && movie.Year <= 1999)
+    .map((movie) => movie.Title);
+};
 
 /* ESERCIZIO 16
   Scrivi una funzione chiamata "sumAllTheYears" che ritorna la somma di tutti gli anni in cui sono stati prodotti i film contenuti nell'array "movies" fornito.
@@ -455,3 +495,8 @@ const movies = [
       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg",
   },
 ];
+
+console.log(newestMovie(movies));
+console.log(countMovies(movies));
+console.log(onlyTheYears(movies));
+console.log(onlyInLastMillennium(movies));
